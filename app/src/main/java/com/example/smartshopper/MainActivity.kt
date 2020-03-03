655418d89ae01d14fe28651a85b7005af862d9b0
Dhruv Tarpara <tarpara.dhruv@dal.ca>
2020-03-02 23:46:50 -0400

    implemented basic barcode reader

---
+++ app/src/main/java/com/example/smartshopper/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.smartshopper
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            var scanner = IntentIntegrator(this)
+            scanner.setOrientationLocked(false)
+            scanner.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, data: Intent?
+    ) {
+        if (resultCode == Activity.RESULT_OK) {
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    textView.text = result.contents.toString()
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG)
+                        .show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+}
