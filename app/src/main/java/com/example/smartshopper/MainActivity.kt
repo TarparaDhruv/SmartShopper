402844737c4bdf80fdff4fdbe63b44eb3a69278b
Dhruv Tarpara <tarpara.dhruv@dal.ca>
2020-03-07 16:59:54 -0400

    implemented recycler view

--- app/src/main/java/com/example/smartshopper/MainActivity.kt
+++ app/src/main/java/com/example/smartshopper/MainActivity.kt
@@ -5,20 +5,40 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            var scanner = IntentIntegrator(this)
+            val scanner = IntentIntegrator(this)
             scanner.setOrientationLocked(false)
             scanner.initiateScan()
         }
+
+        linearLayoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recyclerView.layoutManager = linearLayoutManager
+
+        val ll = ArrayList<String>()
+        ll.add("1")
+        ll.add("2")
+        ll.add("2")
+        ll.add("2")
+        ll.add("2")
+        ll.add("2")
+        ll.add("2")
+        ll.add("2")
+
+        recyclerView.adapter = RecyclerAdapter(ll)
+
     }
 
     override fun onActivityResult(
