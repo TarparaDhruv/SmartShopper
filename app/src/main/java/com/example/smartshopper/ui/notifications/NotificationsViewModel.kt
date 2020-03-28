3a81eb12e57f8e86685e1868f3c09c0194e3ea00
Nishant Amoli <nishant.amoli@dal.ca>
2020-03-28 12:05:12 -0300

    Navigation Graph Activity committed

---
+++ app/src/main/java/com/example/smartshopper/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.smartshopper.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is User Profile Fragment"
+    }
+    val text: LiveData<String> = _text
+}
