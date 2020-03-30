1c45adae2614120c8947c9ff1d0be58f3738f94c
ponangi <sriram.ponangi@dal.ca>
2020-03-30 18:20:09 -0300

    Integrating Nishant's navigation code.

--- app/src/main/java/com/example/smartshopper/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/smartshopper/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is user profile Fragment"
+        value = "This is user's profile Fragment"
     }
     val text: LiveData<String> = _text
 }
