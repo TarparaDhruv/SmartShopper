2e85bafdac254d40daf030cfd337be3e1753fec9
Dhruv Tarpara <tarpara.dhruv@dal.ca>
2020-04-02 23:27:16 -0300

    added fonts removed unnecessary files renamed classes fixed warnings added loading screen in search hit

--- app/src/main/java/com/example/smartshopper/HomeFragment.kt
+++ app/src/main/java/com/example/smartshopper/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.smartshopper
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
@@ -28,17 +30,25 @@ class HomeFragment : Fragment() {
         v = inflater.inflate(R.layout.fragment_home, container, false)
         linearLayoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
 
-        v.searchbar.searchEditText.setOnEditorActionListener { v, actionId, event ->
+        v.searchbar.searchEditText.setOnEditorActionListener { vv, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                Toast.makeText(context, "search hit", Toast.LENGTH_SHORT).show()
 
+                Toast.makeText(context, "search hit", Toast.LENGTH_SHORT).show()
+                v.loding_gif.visibility = View.VISIBLE
+                Glide.with(requireContext()).load(R.drawable.store_2).into(v.loding_gif)
+                var handler = Handler()
+                val r = Runnable {
+                    v.loding_gif.visibility = View.GONE
+                    //what ever you do here will be done after 3 seconds delay.
+                }
+                handler.postDelayed(r, 3000)
+                //hide virtual keyboard
                 val inp: InputMethodManager =
                     activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 inp.hideSoftInputFromWindow(searchEditText.windowToken, 0)
                 return@setOnEditorActionListener true
             }
             return@setOnEditorActionListener false
-
         }
         v.recyclerView.layoutManager = linearLayoutManager
         val ll = ArrayList<String>()
